4f51f7a21c8ecb99a65e3f0b02f86eb798c7ec8a
Eric Schlaepfer <schlae@users.noreply.github.com>
2024-09-25 20:41:09 -0700

    Get rid of autopull for now

--- pmemtest.c
+++ pmemtest.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 // Our assembled program:
@@ -10,6 +11,8 @@ int main() {
     uint pin = 0;
     uint offset;
     uint sm;
+    uint16_t addr;
+    uint8_t db = 0;
 
     stdio_init_all();
     printf("Test.\n");
@@ -19,10 +22,15 @@ int main() {
     pio_sm_set_enabled(pio, sm, true);
 
     while(1) {
-        pio_sm_put(pio, sm, 0x76543210l);
+        addr++;
+        db++;
+        pio_sm_put(pio, sm, (addr & 0xff) | // Row address
+                            (addr & 0xff00) | // Column address
+                            ((db & 1) << 16)); // Data bit
+//        pio_sm_put(pio, sm, (0xff << 9) | (1 << 17));
         while (pio_sm_is_tx_fifo_full(pio, sm)) {}
-        pio_sm_put(pio, sm, 0xfedcba98l);
-        while (pio_sm_is_tx_fifo_full(pio, sm)) {}
+//        pio_sm_put(pio, sm, 0xfedcba98l);
+//        while (pio_sm_is_tx_fifo_full(pio, sm)) {}
     }
 
     return 0;
