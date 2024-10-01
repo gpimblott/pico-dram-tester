d9fb8e96c4f148b5b1f286ee966a6431b94d9d4f
Eric Schlaepfer <schlae@users.noreply.github.com>
2024-09-30 17:44:42 -0700

    RAM read and write functions

--- pmemtest.c
+++ pmemtest.c
@@ -5,14 +5,44 @@
 // Our assembled program:
 #include "pmemtest.pio.h"
 
+PIO pio;
+uint sm = 0;
+
+// Routines for reading and writing memory.
+int ram_read(int addr)
+{
+        pio_sm_put(pio, sm, 0 | // Fast page mode flag
+                            0 << 1 | // Write flag
+                            (addr & 0xff) << 2 |  // Row address
+                            (addr & 0xff00) << 2| // Column address
+                            ((0 & 1) << 18));    // Data bit
+        while (pio_sm_is_tx_fifo_full(pio, sm)) {}
+        return pio_sm_get(pio, sm) & 0x1;
+}
+
+void ram_write(int addr, int data)
+{
+        pio_sm_put(pio, sm, 0 | // Fast page mode flag
+                            1 << 1 | // Write flag
+                            (addr & 0xff) << 2 |  // Row address
+                            (addr & 0xff00) << 2| // Column address
+                            ((data & 1) << 18));    // Data bit
+        while (pio_sm_is_tx_fifo_full(pio, sm)) {}
+        pio_sm_get(pio, sm); // FIXME: do something with the data bit
+}
+
+// Also need a ram address mapper, do i track ras and cas separately?
+
+// Also need some testing algorithms... :)
 
 int main() {
-    PIO pio;
+//    PIO pio;
     uint pin = 0;
     uint offset;
-    uint sm;
+//    uint sm;
     uint16_t addr;
     uint8_t db = 0;
+    int i;
 
     stdio_init_all();
     printf("Test.\n");
@@ -22,6 +52,7 @@ int main() {
     pio_sm_set_enabled(pio, sm, true);
 
     while(1) {
+#if 0
         addr++;
         db++;
         pio_sm_put(pio, sm, 0 | // Fast page mode flag
@@ -31,6 +62,13 @@ int main() {
                             ((db & 1) << 18));    // Data bit
         while (pio_sm_is_tx_fifo_full(pio, sm)) {}
         pio_sm_get(pio, sm); // FIXME: do something with the data bit
+#endif
+        for (i = 0; i < 5; i++) {
+            db++;
+            ram_read(i);
+            ram_write(i, db & 1);
+            ram_read(i);
+        }
     }
 
     return 0;
