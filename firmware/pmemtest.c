f95f73a98be4e002f1f687cd991e1a8772825b12
Eric Schlaepfer <schlae@users.noreply.github.com>
2024-11-19 20:41:03 -0800

    Add 4416 and 4464

--- firmware/pmemtest.c
+++ firmware/pmemtest.c
@@ -49,8 +49,9 @@ gui_listbox_t *cur_menu;
 char *main_menu_items[MAIN_MENU_ITEMS];
 gui_listbox_t main_menu = {7, 40, 220, MAIN_MENU_ITEMS, 4, 0, 0, main_menu_items};
 
-#define NUM_CHIPS 4
-const mem_chip_t *chip_list[] = {&ram4116_chip, &ram4164_chip, &ram41256_chip, &ram44256_chip};
+#define NUM_CHIPS 6
+const mem_chip_t *chip_list[] = {&ram4116_chip, &ram4164_chip, &ram41256_chip,
+                                 &ram4416_chip, &ram4464_chip, &ram44256_chip};
 
 gui_listbox_t speed_menu = {7, 40, 220, 0, 4, 0, 0, 0};
 
